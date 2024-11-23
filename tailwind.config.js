bfdd41bccb5391ffc34f06326901263255d8da3b
yorgs <94930102+yorgof@users.noreply.github.com>
2024-11-23 11:13:29 -0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+export default {
+  content: [
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
