8b25cf3d8c1e70c68792102f3b3855e74ed0ad38
yorgs <94930102+yorgof@users.noreply.github.com>
2024-11-23 11:13:35 -0800

    Create app.vue

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <header>
+      <GlobalHeader/>
+    </header>
+    <main>
+      <NuxtPage/>
+    </main>
+    <footer>
+      <GlobalFooter/>
+    </footer>
+  </div>
+</template>
+
+<script setup>
+// Global script
+import GlobalFooter from "~/components/global-footer.vue";
+import GlobalHeader from "~/components/global-header.vue";
+</script>
+
+<style scoped>
+/* Global styles */
+</style>
