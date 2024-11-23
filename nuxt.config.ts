a51c2d5e1fa9964d0b943b08c987eafeb8f65eb4
yorgs <94930102+yorgof@users.noreply.github.com>
2024-11-23 11:13:18 -0800

    Create nuxt.config.ts

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  css: [
+    '~/assets/css/tailwind.css',
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
